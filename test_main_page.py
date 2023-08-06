1da409e64f131b89ff760673e8c9986f2df21a6d
sonya <cjabzdbitp@mail.ru>
2023-08-06 18:04:38 +0100

    add test_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pytest
 
 from .pages.basket_page import BasketPage
@@ -29,3 +30,13 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.is_basket_empty_message()
+=======
+from selenium.webdriver.common.by import By
+
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
+>>>>>>> a8c4863 (add test_guest_can_go_to_login_page)
